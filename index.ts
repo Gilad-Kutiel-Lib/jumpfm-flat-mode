765305f7f9c533006b5db6c462641f4265778780
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-25 19:37:40 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,64 +1,54 @@
-import { JumpFm, Panel, Item } from 'jumpfm-api'
+import { JumpFm, Panel, File } from 'jumpfm-api'
 
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-class Flat {
-    readonly panel: Panel
-    readonly jumpFm: JumpFm
-    readonly maxSize
-    watcher: { close: () => {} }
+const flat = (dir: string, maxSize: number): File[] => {
+    const flatDir = (rootDir: string, res: File[]) => {
+        if (res.length > maxSize) return
 
-    constructor(jumpFm: JumpFm, panel: Panel) {
-        this.jumpFm = jumpFm
-        this.panel = panel
-        this.maxSize = jumpFm.settings.getNum('flatModeMaxSize', 200)
-        panel.listen(this)
-    }
-
-    onPanelCd = () => {
-        const url = this.panel.getUrl()
-        if (url.protocol != 'flat') return
-        const items = this.flat(url.path)
-        this.panel.setItems(items)
-        if (items.length > this.maxSize)
-            this.jumpFm.statusBar.err(
-                'flat', {
-                    txt:
-                    'Flat Err',
-                    dataTitle: 'Too many files to show, not showing all files'
-                },
-                5000
-            )
-    }
-
-    private flat = (dir): Item[] => {
-        const flatDir = (rootDir: string, res: Item[]) => {
-            if (res.length > this.maxSize) return
-
-            fs.readdirSync(rootDir)
-                .map(name => path.join(rootDir, name))
-                .filter(fullPath => fs.existsSync(fullPath))
-                .forEach(fullPath => {
-                    const stat = fs.statSync(fullPath)
-                    if (stat.isDirectory()) flatDir(fullPath, res)
-                    else res.length <= this.maxSize && res.push(this.panel.itemFromPath(fullPath))
+        fs.readdirSync(rootDir)
+            .map(name => path.join(rootDir, name))
+            .filter(fullPath => fs.existsSync(fullPath))
+            .forEach(fullPath => {
+                const stat = fs.statSync(fullPath)
+                if (stat.isDirectory()) flatDir(fullPath, res)
+                else res.length <= maxSize && res.push({
+                    name: fullPath
+                    , path: fullPath
                 })
-        }
-
-        const res = []
-        flatDir(dir, res)
-        return res
+            })
     }
+
+    const res = []
+    flatDir(dir, res)
+    return res
 }
 
 export const load = (jumpFm: JumpFm) => {
-    jumpFm.panels.forEach(panel => new Flat(jumpFm, panel))
-    jumpFm.bindKeys('flatMode', ['r'], () => {
-        const pan = jumpFm.getActivePanel()
-        const url = pan.getUrl()
-        if (url.protocol == 'flat') url.protocol = ''
-        else url.protocol = 'flat'
-        pan.cd(url)
-    }).filterMode([])
+    const maxSize = jumpFm.settings.getNum('flatModeMaxSize', 5000)
+
+    jumpFm.panels.forEach(panel => {
+        panel.onCd(() => {
+            const url = panel.getUrl()
+            if (url.protocol != 'flat') return
+
+            const items = flat(url.path, maxSize)
+            panel.setItems(items)
+            if (items.length <= maxSize) return
+
+            jumpFm.statusBar
+                .msg('flat')
+                .setType('err')
+                .setText('Flat Err')
+                .setTooltip('Too many files to show, not showing all files')
+                .setClearTimeout(5000)
+        })
+        panel.bind('flatMode', ['r'], () => {
+            const url = panel.getUrl()
+            if (url.protocol == 'flat') url.protocol = ''
+            else url.protocol = 'flat'
+            panel.cd(url)
+        })
+    })
 }
